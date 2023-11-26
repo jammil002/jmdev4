0f5f98bdb2466e0c2d4e3c3b1028a8d9d1544214
James Miller <jam1211miller@gmail.com>
2023-11-26 14:52:21 -0600

    Remove react icons and replaced with font version

--- src/components/HireCard.tsx
+++ src/components/HireCard.tsx
@@ -3,15 +3,19 @@ import React from "react";
 interface HireCardProps {
   title: string;
   description: string[];
+  date: string;
 }
 
-const HireCard: React.FC<HireCardProps> = ({ title, description }) => {
+const HireCard: React.FC<HireCardProps> = ({ title, description, date }) => {
   return (
     <div className="p-2">
       <div className="group block max-w-m rounded-lg shadow-lg bg-white">
         <div className="px-6 py-4">
-          <div className="font-bold text-black text-xl mb-2 font-BebasNeue">
+          <div className="font-bold text-black text-xl font-BebasNeue">
             {title}
+          </div>
+          <div className="text-base font-Oswald text-gray-600 italic ml-1">
+            {date}
           </div>
           <ul className="list-disc">
             {description.map((desc, index) => (
--- src/components/KnownLanguages.tsx
+++ src/components/KnownLanguages.tsx
@@ -2,16 +2,6 @@ import React from "react";
 import { useState } from "react";
 import type { ReactNode } from "react";
 import Card from "./Card";
-import {
-  DiCss3,
-  DiHtml5,
-  DiJavascript1,
-  DiPython,
-  DiJava,
-  DiReact,
-  DiGit,
-  DiGithubBadge,
-} from "react-icons/di";
 
 type TooltipProps = {
   children: ReactNode;
@@ -49,19 +39,19 @@ const KnownLanguages = () => {
       <h4 className="font-BebasNeue">Known Languages</h4>
       <div className="flex justify-between pb-2 pt-2">
         <Tooltip text="CSS">
-          <DiCss3 size={40} className="shadow-lg" />
+          <i className="devicon-css3-plain shadow-lg text-3xl"></i>
         </Tooltip>
         <Tooltip text="HTML">
-          <DiHtml5 size={40} className="shadow-lg" />
+          <i className="devicon-html5-plain shadow-lg text-3xl"></i>
         </Tooltip>
         <Tooltip text="JavaScript">
-          <DiJavascript1 size={40} className="shadow-lg" />
+          <i className="devicon-javascript-plain shadow-lg text-3xl"></i>
         </Tooltip>
         <Tooltip text="Python">
-          <DiPython size={40} className="shadow-lg" />
+          <i className="devicon-python-plain shadow-lg text-3xl"></i>
         </Tooltip>
         <Tooltip text="Java">
-          <DiJava size={40} className="shadow-lg" />
+          <i className="devicon-java-plain shadow-lg text-3xl"></i>
         </Tooltip>
       </div>
       <h4 className="font-BebasNeue text-right">
@@ -69,13 +59,13 @@ const KnownLanguages = () => {
       </h4>
       <div className="flex justify-between pb-2 pt-2">
         <Tooltip text="React">
-          <DiReact size={40} className="shadow-lg cursor-grab" />
+          <i className="devicon-react-plain shadow-lg text-3xl"></i>
         </Tooltip>
         <Tooltip text="Git">
-          <DiGit size={40} className="shadow-lg cursor-grab" />
+          <i className="devicon-git-plain shadow-lg text-3xl"></i>
         </Tooltip>
         <Tooltip text="Github">
-          <DiGithubBadge size={40} className="shadow-lg cursor-grab" />
+          <i className="devicon-github-plain shadow-lg text-3xl"></i>
         </Tooltip>
       </div>
       <h4 className="font-BebasNeue">Certificates</h4>
